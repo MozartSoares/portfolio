2b0de90d5bcbccd2544240597d4a4681d6ed60de
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-25 23:18:48 -0300

    - criado seção de projetos e componente Projeto

---
+++ src/components/Projeto/LinkBotao.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LinkBotao = styled.a`
+  color: #fff;
+  font-size: 14px;
+  background-color: #4476bf;
+  text-decoratio: none;
+  padding: 8px;
+  display: inline-block;
+  margin-top: 24px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    column-gap: 0;
+    row-gap: 16px;
+  }
+`;
